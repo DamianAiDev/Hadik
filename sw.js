cbd8dfcfc2351986b04af571d8eca0319a1dd12d
DamianAiDev <damian.ai.dev@gmail.com>
2026-05-06 00:33:47 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,21 @@
+const cacheName = 'hadik-v1';
+const assets = [
+  '/Hadik/',
+  '/Hadik/index.html',
+  '/Hadik/manifest.json'
+];
+
 self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(cacheName).then((cache) => cache.addAll(assets))
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((res) => res || fetch(e.request))
+  );
+});self.addEventListener('install', (e) => {
   e.waitUntil(caches.open('v1').then((c) => c.addAll(['/Hadik/', '/Hadik/index.html'])));
 });
 self.addEventListener('fetch', (e) => {
